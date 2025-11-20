ae76f49370c0cc22903a5ea83f52389a84171338
KOCAOGLU Manolia <157006573+manolia-kocaoglu@users.noreply.github.com>
2025-11-20 03:32:24 +0100

    Refactor PocketBase integration and improve project retrieval logic

--- src/utils/pb.ts
+++ src/utils/pb.ts
@@ -1,8 +1,16 @@
 import PocketBase from 'pocketbase';
 import type { TypedPocketBase } from "./pocketbase-types";
-var path='';
-if(import.meta.env.MODE === 'development')
-    path = 'http://localhost:8090'    //localhost = machine de dev
-else path = 'https://portfolio.manolia-kocaoglu.fr:443'   //url du site 
+
+var path = '';
+if (import.meta.env.MODE === 'development') {
+    path = 'http://127.0.0.1:8090';    // localhost = machine de dev
+} else {
+    path = 'https://portfolio.manolia-kocaoglu.fr:443';   // url du site 
+}
+
 const pb = new PocketBase(path) as TypedPocketBase;
+
+// Log pour debug
+console.log('📡 PocketBase configuré sur:', path);
+
 export default pb;
